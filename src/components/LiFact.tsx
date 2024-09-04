89e4d57994832049a5ba8446456051de2e3b7a0a
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-09-04 17:29:13 -0300

    project 1: 2

---
+++ src/components/LiFact.tsx
@@ -0,0 +1,7 @@
+import blueElipse from "../assets/elipse.svg"
+
+export default function LiFact({ text }: { text: string }) {
+    return (
+        <li className="flex flex-row gap-5 items-center"><img src={blueElipse} alt="Item Marker" />{text}</li>
+    )
+}
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,16 @@
+import LiFact from "./LiFact"
+
 export default function Main() {
     return (
-        <main>
-            <h1>Main Element</h1>
+        <main className="bg-react-lightgray text-white flex flex-col items-center h-[100vh]">
+            <h1 className="text-5xl p-16 font-bold">Fun Facts about React</h1>
+            <ul className="text-3xl flex flex-col gap-4">
+                <LiFact text="Was first released in 2013"></LiFact>
+                <LiFact text="Was originally created by Jordan Walke"></LiFact>
+                <LiFact text="Has well over 200k stars on GitHub"></LiFact>
+                <LiFact text="Is maintained by Meta"></LiFact>
+                <LiFact text="Powers thousands of enterprise apps, including mobile apps"></LiFact>
+            </ul>
         </main>
     )
 }
